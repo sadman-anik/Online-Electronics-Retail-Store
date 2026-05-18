5f4ff7a68cb114deec01b01f02194e92608d76a2
Sadman Anik <sadmananik@Sadmans-MacBook-Pro.local>
2026-05-18 15:36:20 +1000

    Add environment start script DB creation check

--- start-retailstore-env.sh
+++ start-retailstore-env.sh
@@ -4,6 +4,11 @@ echo "Starting MySQL..."
 sudo /usr/local/mysql/support-files/mysql.server start
 
 echo ""
+echo "Checking retailestoredb database..."
+MYSQL_USER=${MYSQL_USER:-root}
+mysql -u "$MYSQL_USER" -p -e "CREATE DATABASE IF NOT EXISTS retailestoredb;"
+
+echo ""
 echo "Starting GlassFish..."
 $HOME/EnterpriseApps/glassfish7/bin/asadmin start-domain
 
