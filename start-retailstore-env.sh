d915c19263c58978d651f262c5e174873cc0fc09
SRAVYAMATTA28 <sravya.matta@cqumail.com>
2026-05-19 16:32:11 +1000

    Update start-retailstore-env.sh

--- start-retailstore-env.sh
+++ start-retailstore-env.sh
@@ -8,7 +8,7 @@ echo "Checking retailestoredb database..."
 MYSQL_USER=${MYSQL_USER:-root}
 mysql -u "$MYSQL_USER" -p -e "CREATE DATABASE IF NOT EXISTS retailestoredb;"
    
-echo ""
+echo "" 
 echo "Starting GlassFish..."
 $HOME/EnterpriseApps/glassfish7/bin/asadmin start-domain
    
